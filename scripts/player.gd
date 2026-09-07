c2e1d24962fc07c8dcbd661576a59846db94880e
Slime <amitbro640@gmail.com>
2026-09-07 23:12:44 +0545

    Still finishing player movement.. Smoothing it out and all

--- scripts/player.gd
+++ scripts/player.gd
@@ -11,6 +11,8 @@ var is_dashing = false
 var dash_speed = 1000
 var dash_time = 0.2
 var dash_direction = 1
+var dash_no = 1
+var is_jumping = false
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -21,16 +23,16 @@ func _physics_process(delta: float) -> void:
 	
 	if is_on_floor():
 		jump = 2
+		is_jumping = false
 		
 	
 	if Input.is_action_just_pressed("Jump") and jump>0:
 		velocity.y = JUMP_VELOCITY
 		jump -= 1
-		
-	if Input.is_action_just_pressed("Dash") and not is_dashing:
-		dash()
-	if is_dashing:
-		velocity.x = dash_direction * dash_speed
+		animate_sprite.play("jump")
+		is_jumping = true
+		print("false")
+	
 
 
 	# Die when fall of
@@ -45,26 +47,40 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		
+	
 	if velocity.x > 0.1:
-		animate_sprite.play("run")
+		if not is_jumping:
+			animate_sprite.play("run")
 		dash_direction = 1
 		animate_sprite.flip_h = false
 	elif velocity.x < -0.1:
 		dash_direction = -1
-		animate_sprite.play("run")
+		if not is_jumping:
+			animate_sprite.play("run")
 		animate_sprite.flip_h = true
 	else:
-		animate_sprite.play("idle")
+		if not is_jumping:	
+			animate_sprite.play("idle")
+		
+	if Input.is_action_just_pressed("Dash") and not is_dashing and dash_no == 1:
+		dash()
+		await get_tree().create_timer(1.5).timeout
+		print("Dash reload")
+		dash_no = 1
+	if is_dashing:
+		velocity.x = dash_direction * dash_speed
 
 	move_and_slide()
 	
 func dash():
 	is_dashing = true
-	var direction = Input.get_axis("Left","Right")
-	if direction != 0:
-		dash_direction = direction
-	await get_tree().create_timer(2).timeout
+	dash_no = 0
+	#var direction = Input.get_axis("Left","Right")
+	#if direction != 0:
+		#dash_direction = direction
+	#else:
+		#dash_direction = 1
+	await get_tree().create_timer(dash_time).timeout
 	is_dashing = false
-	print("Dash")
+	
 		
