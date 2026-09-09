6cbaad29724550dcde33c4601c3e11c542b8d1f7
Slime <amitbro640@gmail.com>
2026-09-09 17:33:04 +0545

    Level making

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -11,10 +11,12 @@ func add_coin():
 	coin += 1
 	print(coin)
 
-func _process(_delta):
-	if level == 1:
-		if not key:
-			key_label.text = "Need key!"
-		if key:
-			key_label.text = "You have Key. Next Level?"
+#func _process(_delta):
+	#if level == 1:
+		#if key_label == null:
+			#return
+		#if not key:
+			#key_label.text = "Need key!"
+		#if key:
+			#key_label.text = "You have Key. Next Level?"
 		
