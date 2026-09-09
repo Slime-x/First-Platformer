03689a2ce2e3290fbd887fdb712d501f1fa2babc
Slime <amitbro640@gmail.com>
2026-09-09 14:31:38 +0545

    Sky 2

--- scripts/trampoline_2.gd
+++ scripts/trampoline_2.gd
@@ -6,6 +6,6 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	animate.play("open")
-	body.velocity.y = -800.0
+	body.velocity.y = randf_range(-800,-850)
 	await get_tree().create_timer(0.2).timeout
 	animate.play("close")
